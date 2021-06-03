3ea86b93919fc85e0fdd286aa96d8b06876caaed
AlonsoYuen19 <wwepg15@gmail.com>
2021-06-03 02:30:39 -0500

    Entidad Reviews
    Ingreso la entidad reviews

--- src/main/java/com/uter/entities/Reviews.java
+++ src/main/java/com/uter/entities/Reviews.java
@@ -1,7 +1,34 @@
 package com.uter.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reviews implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
 
+    @Column(name = "description", nullable = false, length = 150)
+    private String description;
+    @Column(name = "stars", nullable = false)
+    private int stars;
+
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="seller_id",nullable = false)
+    private Seller seller;
+
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="customer_id",nullable = false)
+    private Customer customer;
 }
